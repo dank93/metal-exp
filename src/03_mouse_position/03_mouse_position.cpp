06c42169d78da63c8c484b6908890b4eed61a108
Daniel Kurek <dkurek93@gmail.com>
2022-11-02 22:41:59 -0700

    mouse position example

---
+++ src/03_mouse_position/03_mouse_position.cpp
@@ -0,0 +1,8 @@
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include <iostream>
+
+int main() { std::cout << "hi" << std::endl; }
