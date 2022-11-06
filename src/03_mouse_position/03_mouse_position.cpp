e67fc2a9ba98c83542e5e03bcd3bb30ff14a8a02
Daniel Kurek <dkurek93@gmail.com>
2022-11-05 22:36:55 -0700

    try to get mouse position but fail due to accessibility permissions

--- src/03_mouse_position/03_mouse_position.cpp
+++ src/03_mouse_position/03_mouse_position.cpp
@@ -3,6 +3,51 @@
 #define MTK_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 
-#include <iostream>
+#include <ApplicationServices/ApplicationServices.h>
 
-int main() { std::cout << "hi" << std::endl; }
+#include <iostream>
+#include <string>
+
+void fancyPrintLocation(CGPoint location) {
+  std::string clearLastLine = ("\33[2K\r(");
+  std::string printOut = clearLastLine + std::to_string((int)(location.x)) +
+                         std::string(", ") + std::to_string((int)(location.y)) +
+                         std::string(")");
+  std::cout << printOut << std::flush;
+}
+
+CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
+                             CGEventRef event, void *refcon) {
+  if (type != kCGEventMouseMoved) {
+    return event;
+  }
+
+  CGPoint location = CGEventGetLocation(event);
+  fancyPrintLocation(location);
+  return event;
+}
+
+int main() {
+  CGRect screenBounds = CGDisplayBounds(CGMainDisplayID());
+  printf("The main screen is %dx%d\n", (int)screenBounds.size.width,
+         (int)screenBounds.size.height);
+
+  CFMachPortRef eventTap = CGEventTapCreate(
+      kCGSessionEventTap, kCGHeadInsertEventTap, kCGEventTapOptionDefault,
+      (1 << kCGEventMouseMoved), myCGEventCallback, NULL);
+
+  if (!eventTap) {
+    fprintf(stderr, "failed to create event tap :/\n");
+    exit(1);
+  }
+
+  CFRunLoopSourceRef runLoopSource =
+      CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+
+  CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource,
+                     kCFRunLoopCommonModes);
+
+  CGEventTapEnable(eventTap, true);
+
+  CFRunLoopRun();
+}
--- src/03_mouse_position/build.sh
+++ src/03_mouse_position/build.sh
@@ -2,9 +2,4 @@
 
 c++ --std=gnu++20 03_mouse_position.cpp \
     -o 03_mouse_position.out \
-    -I ../../metal_headers/metal-cpp \
-    -I ../../metal_headers/metal-cpp-extensions \
-    -framework Foundation \
-    -framework AppKit \
-    -framework Metal \
-    -framework MetalKit
+    -framework ApplicationServices
